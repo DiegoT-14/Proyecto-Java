47e9bc4b552192bf116d596497bc7d33fd299ec9
DiegoT-14 <diegoalejandro1405@outlook.com>
2026-04-19 19:05:01 -0500

    Add songs list to Playlist class

--- JAVA-Project-main/src/main/java/com.javeriana/model/Playlist.java
+++ JAVA-Project-main/src/main/java/com.javeriana/model/Playlist.java
@@ -1,17 +1,22 @@
 package com.javeriana.model;
 
 import java.util.UUID;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Playlist {
     private UUID id;
     private String name;
+    private List<Song> songs;
 
     public Playlist(String name){
         this.id = UUID.randomUUID();
         this.name = name;
+        this.songs = new ArrayList<>();
     }
 
     public UUID getId() {
+
         return id;
     }
 
@@ -19,8 +24,16 @@ public class Playlist {
         return name;
     }
 
+    public List<Song> getSongs() {
+        return songs;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
     }
 
     @Override
