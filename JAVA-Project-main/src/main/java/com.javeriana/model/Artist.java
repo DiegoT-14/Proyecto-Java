41a463b159f6dc20560c68cf12e95bdfe3e22acb
DiegoT-14 <diegoalejandro1405@outlook.com>
2026-04-28 16:41:01 -0500

    Simplify Artist class methods
    Refactored getter and setter methods for name.

--- JAVA-Project-main/src/main/java/com.javeriana/model/Artist.java
+++ JAVA-Project-main/src/main/java/com.javeriana/model/Artist.java
@@ -15,17 +15,9 @@ public class Artist {
         this.name = name;
     }
 
-    public UUID getId() {
-        return id;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    public UUID getId() { return id; }
+    public String getName(){ return name; }
+    public void setName(String name) { this.name = name; }
 
     @Override
     public String toString() {
